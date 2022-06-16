9fecb245a12540d31cbd75900afdd42a2587c91f
devashree610 <66308945+devashree610@users.noreply.github.com>
2022-06-16 20:07:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask,jsonify
+app=Flask(__name__)
+@app.route('/test',methods=['POST','GET'])
+def test():
+    return jsonify({'response':"hello first"})
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000)
